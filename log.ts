8be778a6f452712871690161380169186bcdd7cf
Kalimandla Soniya <kalimandlasoniya@gmail.com>
2026-04-08 13:29:51 +0530

    Delete log.ts

--- log.ts
+++
@@ -1,9 +0,0 @@
-export function Log(
-  app: string,
-  level: string,
-  type: string,
-  message: string,
-  token: string
-) {
-  console.log(`[${app}] [${level}] [${type}] ${message} (token: ${token})`);
-}
